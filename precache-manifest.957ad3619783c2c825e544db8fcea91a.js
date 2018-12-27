38e02a7f89d3d62c3189418136d68befdd9eb538
Colin <tresscd@gmail.com>
2018-12-27 13:07:27 -0500

    Updates

---
+++ precache-manifest.957ad3619783c2c825e544db8fcea91a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4b0229636e7299a7575d20146e97635",
+    "url": "/taketwo/static/media/tiles.a4b02296.png"
+  },
+  {
+    "revision": "ec0781f9d1bd572059812023187faa0b",
+    "url": "/taketwo/static/media/resume.ec0781f9.pdf"
+  },
+  {
+    "revision": "04182a33cbe8ff716c1241b3efa12f20",
+    "url": "/taketwo/static/media/me.04182a33.jpeg"
+  },
+  {
+    "revision": "b5bfc6205a4fe0a0672998f008540184",
+    "url": "/taketwo/static/media/cylinder.b5bfc620.png"
+  },
+  {
+    "revision": "1908bfdd546ae61967f21557f821286b",
+    "url": "/taketwo/static/media/boxes.1908bfdd.png"
+  },
+  {
+    "revision": "1194bb5764d29e1f9036",
+    "url": "/taketwo/static/js/runtime~main.1194bb57.js"
+  },
+  {
+    "revision": "280be5ae3c5f3a7cb49a",
+    "url": "/taketwo/static/js/main.280be5ae.chunk.js"
+  },
+  {
+    "revision": "ce80dca6b5a14ad641e1",
+    "url": "/taketwo/static/js/1.ce80dca6.chunk.js"
+  },
+  {
+    "revision": "280be5ae3c5f3a7cb49a",
+    "url": "/taketwo/static/css/main.d30c50d5.chunk.css"
+  },
+  {
+    "revision": "f689a0545d727cfffb7cf3e0f22746e2",
+    "url": "/taketwo/index.html"
+  }
+];

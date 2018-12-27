ff9bf7f96dc18a6d212af2b3cbe101a489e4b710
Colin <tresscd@gmail.com>
2018-12-27 15:53:37 -0500

    Add switch to router.

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Projects from './components/projects/projects_container.js';
 import Project from './components/projects/project_container.js';
 import Blog from './components/blog/blog_container.js';
 import About from './components/about/about.jsx';
-import { Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Route exact path="/" render={(props) => { return <Redirect to="/projects" /> }} />
-        <Route exact path="/projects" component={ Projects } />
-        <Route exact path={`/projects/:id`} component={ Project } />
-        <Route exact path="/blog" component={ Blog } />
-        <Route exact path="/about" component={ About } />
+        <Switch>
+          <Route exact path="/" render={(props) => { return <Redirect to="/projects" /> }} />
+          <Route exact path="/projects" component={ Projects } />
+          <Route exact path={`/projects/:id`} component={ Project } />
+          <Route exact path="/blog" component={ Blog } />
+          <Route exact path="/about" component={ About } />
+        </Switch>
       </div>
     );
   }
